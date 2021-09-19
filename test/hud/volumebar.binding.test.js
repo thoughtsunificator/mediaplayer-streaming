dee3729bbc3bff770c7c30ab2165bec90fa6a4f4
thoughtsunificator <85041649+thoughtsunificator@users.noreply.github.com>
2021-09-19 10:27:40 +0200

    mocha

--- test/hud/volumebar.binding.test.js
+++ test/hud/volumebar.binding.test.js
@@ -1,30 +1,31 @@
+import assert from "assert"
 import { JSDOM } from "jsdom"
 import { Core, Binding } from "domodel"
 
-const virtualDOM = new JSDOM()
+const virtualDOM = new JSDOM(``)
 const window = virtualDOM.window
 const { document } = window
 
 const RootModel = { tagName: "div" }
 let rootBinding
 
-export function setUp(callback) {
-	rootBinding = new Binding()
-	Core.run(RootModel, { parentNode: document.body, binding: rootBinding })
-	callback()
-}
+describe("volumebar.binding", () => {
 
-export function tearDown(callback) {
-	rootBinding.remove()
-	callback()
-}
+	beforeEach(() => {
+		rootBinding = new Binding()
+		Core.run(RootModel, { parentNode: document.body, binding: rootBinding })
+	})
 
-export function instance(test) {
-	test.expect(1)
-	test.ok(new Binding() instanceof Binding)
-	test.done()
-}
+	afterEach(() => {
+		rootBinding.remove()
+	})
 
-export function onCreated(test) {
-	test.done()
-}
+	it("instance", () => {
+		assert.ok(new Binding() instanceof Binding)
+	})
+
+	it("onCreated", () => {
+		assert.ok(new Binding() instanceof Binding)
+	})
+
+})
